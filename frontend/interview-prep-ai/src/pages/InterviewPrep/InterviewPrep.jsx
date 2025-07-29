c408b630a47814e22722d3a56158e303997da895
Swati-Kumari-Das <swatikumaridas1111@gmail.com>
2025-07-29 23:28:45 +0530

    added

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -6,7 +6,11 @@ import { LuAlertCircle, LuChevronDown } from "react-icons/lu";
 import SpinnerLoader from "../../components/Loader/SpinnerLoader";
 import { toast } from "react-hot-toast";
 import DashboardLayout from '../../components/layouts/DashboardLayout';
-
+import RoleInfoHeader from './components/RoleInfoHeader';
+import { LuAlertTriangle } from 'react-icons/lu';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import QuestionCard from '../../components/Cards/QuestionCard';
 const InterviewPrep = () => {
     const { sessionId } = useParams();
 
@@ -18,6 +22,7 @@ const InterviewPrep = () => {
 
     const [isLoading, setIsLoading] = useState(false);
     const [isUpdateLoader, setIsUpdateLoader] = useState(false);
+   
 
     // Fetch session data by session id
     const fetchSessionDetailsById = async () => {
@@ -26,11 +31,15 @@ const InterviewPrep = () => {
             // Replace with actual API call
             // const response = await getSessionById(sessionId);
             // setSessionData(response.data);
-            setIsLoading(false);
+              const response = await axiosInstance.get(
+                API_PATHS.SESSION.GET_ONE(sessionId)
+            );
+            if (response.data && response.data.session) {
+                setSessionData(response.data.session);
+            }
+          
         } catch (error) {
-            setErrorMsg("Failed to fetch session details");
-            toast.error("Failed to fetch session details");
-            setIsLoading(false);
+            console.log("Error:",error);
         }
     };
 
@@ -75,6 +84,47 @@ const InterviewPrep = () => {
                 : ""
         }
     />
+   <div className="container mx-auto pt-4 pb-4 px-4 md:px-0">
+   <h2 className="text-lg font-semibold color-black">Interview Q & A</h2>
+  
+    <div className="gird grid-cols-12 gap-4 mt-5 mb-10">
+    <div
+      className={`col-span-12 ${
+        openLearnMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+      }`}
+    >
+      <AnimatePresence>
+        {sessionData?.questions?.map((data, index) => {
+          return (
+            <motion.div
+              key={data._id || index}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, scale: 0.95 }}
+              transition={{
+                duration: 0.4,
+                type: "spring",
+                stiffness: 100,
+                delay: index * 0.1,
+                damping: 15,
+              }}
+              layout
+              layoutId={`question-${data._id || index}`} // Helps Framer track
+            >
+              <QuestionCard
+                question={data?.question}
+                answer={data?.answer}
+                onLearnMore={() => generateConceptExplanation(data.question)}
+                isPinned={data?.isPinned}
+                onTogglePin={() => toggleQuestionPinStatus(data._id)}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
+    </div>
+  </div>
+   </div>
        </DashboardLayout>
     );
 };
